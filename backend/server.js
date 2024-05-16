ac4f2f350bccf921ff8cb1fd24e1bb2916dd0736
Jeff Uhrick <jjuhric@gmail.com>
2024-05-16 18:28:09 -0500

    Adding custom error handling middleware

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
-
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import userRoutes from './routes/userRoutes.js';
 
 const PORT = process.env.PORT || 5000;
@@ -13,6 +13,9 @@ app.use('/api/users', userRoutes);
 app.get('/', (req, res) => {
     res.send('Server is ready');
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 
 app.listen(PORT, () => {
