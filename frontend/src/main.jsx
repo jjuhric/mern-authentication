c227e9575e635fcc41b3a6bd20761dfe7b7b3ba8
Jeff Uhrick <jjuhric@gmail.com>
2024-05-20 01:12:15 -0500

    adding redux

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import store from './store'
+import { Provider } from 'react-redux'
 import App from './App.jsx'
 import Home from './screens/HomeScreen.jsx'
 import Login from './screens/LoginScreen.jsx'
@@ -19,7 +21,9 @@ const router = createBrowserRouter(
 
 )
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
 )
